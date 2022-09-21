f800cc230c7b5c76a602d18675693a84bf0e73dd
Ted O'Connor <ted@opub.com>
2022-09-20 21:25:27 -0400

    added detail popup with sibling tracker

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const maxRows = 500;
 const columns = ['Rank', 'Name', 'Sibling', 'Owner', 'Owns', 'DNA', 'DNA Split', 'Clothing', 'Eyes', 'Hair', 'Mouth', 'Teardrop'];
+const traits = ['Owner', 'Siblings', 'DNA', 'DNA Split', 'Clothing', 'Eyes', 'Hair', 'Mouth', 'Teardrop'];
 
 let data = [];
 (function () {
@@ -18,13 +19,13 @@ function updateTable() {
 }
 
 function populateTable() {
-    if (document.getElementById('rows')) {
-        document.getElementById('rows').remove();
+    if (get('rows')) {
+        get('rows').remove();
     }
 
     // filter results
-    const filter = document.getElementById('search').value.toUpperCase();
-    const sibling = document.getElementById('siblings').checked;
+    const filter = get('search').value.toUpperCase();
+    const sibling = get('siblings').checked;
     let filtered = data.filter(m => {
         if (sibling && m.sibling.length === 0) {
             return false;
@@ -42,7 +43,7 @@ function populateTable() {
         return true;
     });
     let truncated = false;
-    if(filtered.length > maxRows) {
+    if (filtered.length > maxRows) {
         filtered = filtered.slice(0, maxRows);
         truncated = true;
     }
@@ -52,12 +53,14 @@ function populateTable() {
     rows.id = 'rows';
     for (let item of filtered) {
         const row = rows.insertRow(-1);
+        row.dataset.id = item.mint;
+        row.addEventListener('click', showDetails);
         for (let i = 0; i < columns.length; i++) {
             const cell = row.insertCell(i);
             if (columns[i] === 'Name') {
                 cell.innerHTML = `<a href="${item.details}" target="_blank">${item.name}</a>`;
             } else if (columns[i] === 'Owner') {
-                cell.innerHTML = `<a href="https://magiceden.io/u/${item.owner}" title="${item.ownerAlt ? item.ownerAlt : ''}" target="_blank">${item.owner}</a>`;
+                cell.innerHTML = mask(item.owner);
             } else {
                 cell.innerHTML = item.hasOwnProperty(columns[i]) ? item[columns[i]] : item[columns[i].toLowerCase()];
             }
@@ -66,24 +69,19 @@ function populateTable() {
             }
         }
     }
-    if(truncated || filtered.length === 0) {
+    if (truncated || filtered.length === 0) {
         const row = rows.insertRow(-1);
         const cell = row.insertCell(0);
         cell.colSpan = columns.length;
         cell.innerHTML = truncated ? 'Change filter or sort to display additional results.' : 'No matching results found.';
     }
-    const table = document.getElementById('results');
+    const table = get('results');
     table.appendChild(rows);
     loading(false);
 }
 
-function loading(active) {
-    document.getElementById('loader').style.display = active ? 'block' : 'none';
-    document.getElementById('container').style.display = 'block';
-}
-
 function populateHeader() {
-    const header = document.getElementById('header');
+    const header = get('header');
     const row = header.insertRow(-1);
     for (let col of columns) {
         const th = document.createElement('th');
@@ -98,6 +96,40 @@ function populateHeader() {
         div.appendChild(caret);
         th.appendChild(div);
         row.appendChild(th);
+    }
+}
+
+function toggleArrow(event) {
+    const element = event.target;
+    let caret, field, reverse;
+    if (element.tagName === 'I') {
+        caret = element;
+        field = element.parentElement.id;
+    } else {
+        caret = element.getElementsByClassName('caret')[0];
+        field = element.id;
+    }
+
+    const sortUp = 'fa fa-caret-up';
+    const sortDown = 'fa fa-caret-down';
+    const showing = caret.className;
+    clearArrow();
+    if (showing.includes(sortUp)) {
+        caret.className = `caret ${sortDown}`;
+        reverse = false;
+    } else {
+        caret.className = `caret ${sortUp}`;
+        reverse = true;
+    }
+
+    data.sort(sortBy(field, reverse));
+    updateTable();
+}
+
+function clearArrow() {
+    let carets = document.getElementsByClassName('caret');
+    for (let caret of carets) {
+        caret.className = 'caret fa fa-sort';
     }
 }
 
@@ -116,39 +148,78 @@ const sortBy = (field, reverse) => {
     };
 };
 
-function clearArrow() {
-    let carets = document.getElementsByClassName('caret');
-    for (let caret of carets) {
-        caret.className = 'caret fa fa-sort';
-    }
+function loading(active) {
+    get('loader').style.display = active ? 'block' : 'none';
+    get('container').style.display = 'block';
 }
 
-function toggleArrow(event) {
-    let element = event.target;
-    let caret, field, reverse;
-    if (element.tagName === 'I') {
-        caret = element;
-        field = element.parentElement.id;
+function showDetails(event) {
+    let mint;
+    if (typeof event === 'string') {
+        mint = event;
     } else {
-        caret = element.getElementsByClassName('caret')[0];
-        field = element.id;
+        let target = event.target;
+        while (!target.dataset.id) {
+            target = target.parentElement;
+        }
+        mint = target.dataset.id;
     }
-
-    const sortUp = 'fa fa-caret-up';
-    const sortDown = 'fa fa-caret-down';
-    let showing = caret.className;
-    clearArrow();
-    if (showing.includes(sortUp)) {
-        caret.className = `caret ${sortDown}`;
-        reverse = false;
-    } else {
-        caret.className = `caret ${sortUp}`;
-        reverse = true;
-    }
-
-    data.sort(sortBy(field, reverse));
-    updateTable();
+    const nft = data.find(item => item.mint === mint);
+    console.log('showing', nft);
+    get('details-name').innerHTML = `${nft.name} (Rank ${nft.rank})`;
+    get('details-image-link').href = nft.image;
+    get('details-image').src = nft.image;
+    get('details-traits').innerHTML = getTraits(nft);
+    get('details').style.display = 'block';
 }
 
-document.getElementById('search').addEventListener('keyup', updateTable);
-document.getElementById('siblings').addEventListener('click', updateTable);
+function getTraits(nft) {
+    let html = '';
+    for (let trait of traits) {
+        let value = nft[trait] ? nft[trait] : nft[trait.toLowerCase()];
+        if (value) {
+            if (trait === 'Owner') {
+                value = `<a href="https://magiceden.io/u/${value}" title="${value}" target="_blank">${mask(value)}${nft.ownerAlt ? ' (' + nft.ownerAlt + ')' : ''}</a>`;
+            } else if (trait === 'Siblings') {
+                if (value.length === 0) {
+                    value = 'None</div><br><div>';
+                } else {
+                    let links = '';
+                    for (let sib of value) {
+                        const twin = data.find(item => item.mint === sib);
+                        links += `<a href="javascript:showDetails('${twin.mint}')">${twin.name}</a> `;
+                    }
+                    value = links + '</div><br><div>';
+                }
+            }
+            html += `<div class="trait"><span class="title">${trait}: </span>${value}</div>`;
+        }
+    }
+    return html;
+}
+
+function mask(address) {
+    return address ? `${address.substring(0, 5)}...${address.substring(40)}` : '';
+}
+
+function hideDetails() {
+    get('details').style.display = 'none';
+}
+
+function get(id) {
+    const elem = document.getElementById(id);
+    if (!elem) {
+        console.error('unable to find element with id', id);
+    }
+    return elem;
+}
+
+get('search').addEventListener('keyup', updateTable);
+get('siblings').addEventListener('click', updateTable);
+
+document.addEventListener('click', hideDetails, true);
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        hideDetails();
+    }
+});
